853e1d66994a00516663bff97d7a9f2b61e9a6e3
Michael Wrobel <michaelrwrobel@sbcglobal.net>
2018-04-27 23:13:20 -0400

    Corrections for the survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -11,25 +11,25 @@ var friends = [
       routeName: "jim",
       name: "Jim",
       photo: "",
-      scores: [1,2,3,4,5,5,4,3,2,1] 
+      scores: [3,2,1,2,5,1,4,2,2,1] 
     },
     {
       routeName: "jessica",
       name: "Jessica",
       photo: "",
-      scores: [1,2,3,4,5,5,4,3,2,1] 
+      scores: [1,2,3,4,1,1,1,3,2,5] 
     },
     {
       routeName: "morgan",
       name: "Morgan",
       photo: "",
-      scores: [1,2,3,4,5,5,4,3,2,1] 
+      scores: [3,5,5,4,5,5,4,3,2,4] 
     },
     {
       routeName: "mike",
       name: "Mike",
       photo: "",
-      scores: [1,2,3,4,5,5,4,3,2,1] 
+      scores: [1,2,2,4,2,5,1,3,2,1] 
     }
   ];
 
